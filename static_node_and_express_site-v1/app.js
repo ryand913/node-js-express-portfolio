0cb9d3ce002889cfd52180e82c37d737a0188e12
ryand913 <98771607+ryand913@users.noreply.github.com>
2022-06-15 15:37:39 -0400

    test

--- static_node_and_express_site-v1/app.js
+++ static_node_and_express_site-v1/app.js
@@ -1,7 +1,21 @@
-const express = require('Express');
-const data = require("../data/projectData.json");
+const express = require('express');
+// const data = require("../data/projectData.json");
 
 const app = express();
 
 app.set('view engine', 'pug');
 
+app.use('/static', express.static('public'));
+
+app.get('/', (req, res) => {
+    res.send('<h1>Future site of really cool stuff</h1>');
+});
+
+app.get('/', (req,res) => {
+   const data = res.locals.data.projects;
+   res.render(data)
+});
+
+app.listen(3001, () => {
+    console.log('app is running on 3000');
+});
