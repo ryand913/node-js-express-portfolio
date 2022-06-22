70f8f705a32879503e3618e275b564b4aaa7d0f6
ryand913 <98771607+ryand913@users.noreply.github.com>
2022-06-21 20:42:51 -0400

    updates

--- static_node_and_express_site-v1/app.js
+++ static_node_and_express_site-v1/app.js
@@ -11,13 +11,15 @@ app.set('view engine', 'pug');
 app.use('/static', express.static('public'));
 
 
+
 app.get('/', (req,res) => {
     res.locals = projects;
    res.render('index', projects);
-
+    // next();
 });
 app.get('/about', (req,res) => {
     res.render('about');
+    // next();
 });
 
 app.get('/projects/:id', (req,res) => {
@@ -25,9 +27,29 @@ app.get('/projects/:id', (req,res) => {
     const { id } = req.params;
 
     res.render('project', { projects, id });
+    // next();
 });
 app.listen(3000, () => {
     console.log('test');
 });
+
+
+app.use((req, res, next) => {
+    const err = new Error('Not Found slut');
+    err.status = 'helpppp';
+    next(err);
+  });
+
+  
+  app.use((err, req, res, next) => {
+    res.locals.error = err;
+    res.status(err.status)
+    res.render('error', {err});
+  });
+
+//   app.use((err,req,res,next) => {
+//       res.status(500);
+//       res.render('error', { error: err})
+//   });
 
 module.exports = router;
